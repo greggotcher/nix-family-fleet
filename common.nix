6ef787a6af1b8c28ad4e7105f501e754864adbf9
Greg Gotcher <greg@gotcherfamily.com>
2025-12-14 09:47:29 -0600

    Add Microsoft Remote Desktop RDP client to all macs

--- common.nix
+++ common.nix
@@ -59,6 +59,7 @@
       "vlc"              # Media player (not available for macOS in nixpkgs)
       "libreoffice"      # Office suite (not available for macOS in nixpkgs)
       "tailscale-app"    # VPN mesh network for all computers
+      "microsoft-remote-desktop"  # RDP client for Windows machines
     ];
 
     # -------------------------------------------------------------------------
